c23a7c99bf0ca0edd55c0587e8e989bd50636792
mohammedshanaef <mohammed.shaneaf.963@gmail.com>
2025-01-16 12:28:21 +0300

    Ui Splash View Body Done

--- lib/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:doctor_consultant/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
@@ -6,44 +8,57 @@ class SplashViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Color(0xFF61CEFF),
-              Color(0xFFFFFFFF),
-              Color(0xFFFFFFFF),
-              Color(0xFF0EBE7E),
-            ],
-            stops: [0.0, 0.3, 0.8, 1.0],
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.local_hospital,
-                size: 100,
-                color: Color(0xFF00BFA5),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Doctor Hunt',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
+      body: LinearColorInSplash(),
+    );
+  }
+}
+
+class LinearColorInSplash extends StatelessWidget {
+  const LinearColorInSplash({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color(0xFF61CEFF),
+            Color(0xFFFFFFFF),
+            Color(0xFFFFFFFF),
+            Color(0xFF0EBE7E),
+          ],
+          stops: [0.1, 0.3, 0.7, 1.0],
         ),
       ),
+      child: Center(
+        child: BodyWidget(),
+      ),
+    );
+  }
+}
+
+class BodyWidget extends StatelessWidget {
+  const BodyWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 50,
+        ),
+        Lottie.asset(
+          AssetData.splashAnimation,
+          reverse: true,
+        ),
+      ],
     );
   }
 }
---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,7 @@
+// Utils Folder
+//The utils (utilities) folder inside the core folder is used to store helper functions or classes that perform common, reusable tasks. These utilities are not feature-specific and can be shared across the app.
+
+class AssetData {
+  static const logo = 'assets/images/Beganing/Logo.svg';
+  static const splashAnimation = 'assets/images/Beganing/splash_animation.json';
+}
